b6c924217ed4b03c8313cb90956b6c583f285953
ChristianCoC <christianelmillo81@gmail.com>
2023-10-09 18:25:29 -0300

    Clase Angular UTN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponentComponent},
+  {path: 'contact', component: ContactComponentComponent},
+  {path: 'about', component: AboutUsComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: '**', component: HomeComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    ContactComponentComponent,
+    AboutUsComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
